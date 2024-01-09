c71241ac388ab1a5ed25cc93b5040be157039294
RejaNSU <106128811+RejaNSU@users.noreply.github.com>
2024-01-08 16:41:47 -0800

    Add files via upload

--- Maximum Number of Words Found in Sentences.py
+++ Maximum Number of Words Found in Sentences.py
@@ -1,22 +1,22 @@
 from typing import List
 
-class Solution(object):
-    def mostWordsFound(self, sentences: List[str]) -> int:
-        """
-        :type sentences: List[str]
-        :rtype: int
-        """
-        ans = 0
 
+class Solution:
+    def mostWordsFound(self, sentences):
+        max_len = 0
 
-        for i in range(len(sentences)):
-            sentences[i] = sentences[i].count(' ') + 1
-            ans = max(ans,sentences[i])
+        for curr_sent in sentences:
+            word_list = curr_sent.split(" ")
 
-        return ans
+            curr_len = len(word_list)
+
+            if max_len < curr_len:
+                max_len = curr_len
+
+        return max_len
 
 
 sol = Solution()
 res1 = sol.mostWordsFound(["alice and bob love leetcode", "i think so too", "this is great thanks very much"])
 res2 = sol.mostWordsFound(["please wait", "continue to fight", "continue to win"])
-print(res2)
+print(res1)
